0c62a1a7c29d4774a03bb6122ae816e30fe132c3
salihky1 <135813951+salihky1@users.noreply.github.com>
2024-03-24 01:50:24 +0300

    i

---
+++ js/custom/custom.js
@@ -0,0 +1,40 @@
+//benim sayfam yüklendiğinde çalıştır--domcontentloeded
+//for navbar javascript codes
+window.addEventListener("DOMContentLoaded",event=>{
+    var navbarMobile=function(){
+        const nCollapsible=document.body.querySelector("#mainNavbar");
+        if (!nCollapsible) {
+            return;
+     }
+     if (window.scrollY===0) {
+        nCollapsible.classList.remove("navbar-mobile");
+    
+     }else{
+        nCollapsible.classList.add("navbar-mobile")
+     }
+    };
+    //aşağı inince navbar arkası değişmesi için
+    navbarMobile();
+    document.addEventListener("scroll",navbarMobile);
+    const myNavbar=document.body.querySelector("#mainNavbar");
+    if(myNavbar){
+        new bootstrap.ScrollSpy(document.body,{
+            terget:"#mainNavbar",
+            offset:74,
+        });
+    }
+});
+
+var BtnCanvas =document.querySelectorAll(".btn-close-canvas");
+for (let i =0;i<BtnCanvas.length;i++){
+    BtnCanvas[i].addEventListener("click",function(){
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
+
+
+
+
+
+
+
